d0014ee0492a07f49ad10a31d250d2f66d103dc1
Heliacamara <camara.helia@escolar.ifrn.edu.br>
2025-10-01 17:44:30 -0300

    Carga do elevador

---
+++ primeiros exercecicios do 3B-IP/exercicioV5.1.py
@@ -0,0 +1,19 @@
+# @cikey f8a3d76c7a6f58f18a695f0148380a0a
+# @sid 20251174010003
+# @aid V5.1
+
+
+#begin_inputs
+ 
+
+#end_inputs
+soma= 0
+while True:
+    peso = int(input())
+    soma += peso
+    if soma> 500:          
+        print("Peso excedido")
+        break
+
+
+	
---
+++ primeiros exercecicios do 3B-IP/exercicioV5.2.py
@@ -0,0 +1,11 @@
+# @cikey f8a3d76c7a6f58f18a695f0148380a0a
+# @sid 20251174010003
+# @aid V5.2
+
+
+#begin_inputs
+
+minutos=
+#end_inputs
+
+	
