9f22958b0ca8f19e975bdf9b7304d1c97f2af11f
Heliacamara <camara.helia@escolar.ifrn.edu.br>
2025-10-07 14:28:49 -0300

    IP

--- primeiros exercecicios do 3B-IP/exercicioV4.1.py
+++ primeiros exercecicios do 3B-IP/exercicioV4.1.py
@@ -1,11 +1,11 @@
 # @cikey f8a3d76c7a6f58f18a695f0148380a0a
 # @sid 20251174010003
 # @aid V4.1
-
+ 
 
 #begin_inputs
-x= int(input("Digite o valor de x:"))
-y= int(input("Digite o valor de y:"))
+x=int(input("Digite o valor de x:"))
+y=int(input("Digite o valor de y:"))
 #end_inputs
 
 if x > y:
---
+++ primeiros exercecicios do 3B-IP/exercicioV6.1.py
@@ -0,0 +1,12 @@
+# @cikey f8a3d76c7a6f58f18a695f0148380a0a
+# @sid 20251174010003
+# @aid V6.1
+
+
+#begin_inputs
+
+#end_inputs
+
+import random
+for n in range(6):
+   print(random.randint(1,51))	
