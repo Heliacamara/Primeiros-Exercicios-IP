bcd2f04804a7d8b78bcbd7a563cd33d6215051cf
Heliacamara <camara.helia@escolar.ifrn.edu.br>
2025-11-25 11:50:42 -0300

    Create exercicioV4.2.py

---
+++ exercicioV4.2.py
@@ -0,0 +1,15 @@
+# @cikey f8a3d76c7a6f58f18a695f0148380a0a
+# @sid 20251174010003
+# @aid V4.2
+
+
+#begin_inputs
+ #mantenha esse trecho. o input será manipulado aqui.
+#end_inputs
+contador=0
+for i in range(10):
+    idade=int(input())
+    if idade>=18:
+        contador+=1
+
+print(contador)        
